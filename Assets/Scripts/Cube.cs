699e0a2fdcaf8fa9fc4ec40f0a4ead14977a3150
Aleksandr-Nvk <alexandr.novik2@gmail.com>
2025-05-21 21:16:29 +0300

    Positional movement added

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
@@ -11,9 +12,39 @@ public class Cube : MonoBehaviour
 
     private void Awake()
     {
-        _inputManager.OnTopRightSwipe += () => { };
-        _inputManager.OnTopLeftSwipe += () => { };
-        _inputManager.OnBottomRightSwipe += () => { };
-        _inputManager.OnBottomLeftSwipe += () => { };
+        _inputManager.OnTopRightSwipe += () => Move(MoveType.TopRight);
+        _inputManager.OnTopLeftSwipe += () => Move(MoveType.TopLeft);
+        _inputManager.OnBottomRightSwipe += () => Move(MoveType.BottomRight);
+        _inputManager.OnBottomLeftSwipe += () => Move(MoveType.BottomLeft);
+    }
+
+    private void Move(MoveType moveType)
+    {
+        var endPosition = transform.position;
+        switch (moveType)
+        {
+            case MoveType.TopRight:
+                endPosition.x -= 1;
+                break;
+            case MoveType.TopLeft:
+                endPosition.z -= 1;
+                break;
+            case MoveType.BottomRight:
+                endPosition.z += 1;
+                break;
+            case MoveType.BottomLeft:
+                endPosition.x += 1;
+                break;
+        }
+
+        transform.DOMove(endPosition, 0.5f, true);
+    }
+    
+    private enum MoveType
+    {
+        TopRight,
+        TopLeft,
+        BottomRight,
+        BottomLeft
     }
 }
