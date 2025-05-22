fab4a8d33672852bd82b3646f91462574f3c0160
Oleksandr Novik <alexandr.novik2@gmail.com>
2025-05-23 02:44:15 +0300

    Generate proper grid

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,24 +1,35 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField]
     private GameObject GroundTile;
     
-    private const int GridWidth = 40;
-    private const int GridLength = 60;
+    private const int InitGridWidth = 24;
+    private const int InitGridLength = 70;
 
     private const float HoleProbability = 0.05f;
 
     public void Start()
     {
-        for (var x = 0; x < GridLength; x++)
+        for (var i = 0; i < InitGridLength; i++)
         {
-            for (var z = x; z < GridWidth + x; z++)
-            {
-                if (Random.value < HoleProbability) continue;
-                Instantiate(GroundTile, transform.position + new Vector3(x, 0, z), Quaternion.identity, transform);
-            }
+            SpawnRow(i / 2,  i / 2 + i % 2);
         }
+    }
+
+    private void SpawnRow(int x, int z)
+    {
+        for (var i = 0; i < InitGridWidth; i++)
+        {
+            SpawnTile(x + InitGridWidth - i, z + i + 1);
+        }
+    }
+    
+    private void SpawnTile(int x, int z)
+    {
+        if (Random.value < HoleProbability) return;
+        Instantiate(GroundTile, transform.position + new Vector3(x, 0, z), Quaternion.identity, transform);
     }
 }
