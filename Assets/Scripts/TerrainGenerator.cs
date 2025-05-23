3c33f0ab123fafb73834801f52f345dde73ac8c7
Oleksandr Novik <alexandr.novik2@gmail.com>
2025-05-23 04:28:42 +0300

    Better generation

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,35 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class TerrainGenerator : MonoBehaviour
 {
     [SerializeField]
-    private GameObject GroundTile;
+    private GameObject RegularGroundTile;
+    [SerializeField]
+    private GameObject BorderGroundTile;
+    [SerializeField]
+    private GameObject ObstacleGroundTile;
+    
+    [SerializeField]
+    private Transform Cube;
+
+    private readonly (TileType, float)[] _tileProbabilities = 
+    {
+        (TileType.Hole, 0.05f),
+        (TileType.Obstacle, 0.05f),
+        (TileType.Regular, 0.9f)
+    };
     
     private const int InitGridWidth = 24;
     private const int InitGridLength = 70;
 
     private const float HoleProbability = 0.05f;
 
+    private const float Proximity = 2f;
+    
+    private readonly List<List<Tile>> _rows = new(InitGridLength);
+
     public void Start()
     {
         for (var i = 0; i < InitGridLength; i++)
         {
-            SpawnRow(i / 2,  i / 2 + i % 2);
+            var row = SpawnRow(i / 2, i / 2 + i % 2);
+            _rows.Add(row);
         }
     }
 
-    private void SpawnRow(int x, int z)
+    private List<Tile> SpawnRow(int x, int z)
     {
+        var row = new List<Tile>(InitGridWidth);
         for (var i = 0; i < InitGridWidth; i++)
         {
-            SpawnTile(x + InitGridWidth - i, z + i + 1);
+            var tileType = i is 0 or InitGridWidth - 1
+                ? TileType.Border
+                : GetRouletteTileType(_tileProbabilities);
+            var tile = SpawnTile(x + InitGridWidth - i, z + i + 1, tileType);
+            row.Add(tile);
         }
+
+        return row;
     }
     
-    private void SpawnTile(int x, int z)
+    private Tile SpawnTile(int x, int z, TileType type)
     {
-        if (Random.value < HoleProbability) return;
-        Instantiate(GroundTile, transform.position + new Vector3(x, 0, z), Quaternion.identity, transform);
+        GameObject tilePrefab;
+        switch (type)
+        {
+            case TileType.Regular:
+                tilePrefab = RegularGroundTile;
+                break;
+            case TileType.Border:
+                tilePrefab = BorderGroundTile;
+                break;
+            case TileType.Obstacle:
+                tilePrefab = ObstacleGroundTile;
+                break;
+            case TileType.Hole:
+                return new Tile(null, type);
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+        
+        var tileObject = Instantiate(tilePrefab, transform.position + new Vector3(x, 0, z), Quaternion.identity, transform);
+        return new Tile(tileObject, type);
+    }
+    
+    // shamelessly stolen from https://gamedev.stackexchange.com/a/153851
+    private static TileType GetRouletteTileType((TileType, float)[] weights)
+    {
+        var total = weights.Select(weight => weight.Item2).Sum();
+        if (total <= 0f) throw new ArgumentException("Total weight must be positive.");
+
+        var amount = (float)(new System.Random().NextDouble() * total);
+        var cumulative = 0f;
+        for (var i = 0; i < weights.Length; i++)
+        {
+            cumulative += weights[i].Item2;
+            if (amount <= cumulative) return weights[i].Item1;
+        }
+
+        return weights.Last().Item1;
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public struct Tile
+{
+    public GameObject GameObject;
+    public TileType Type;
+
+    public Tile(GameObject gameObject, TileType type)
+    {
+        GameObject = gameObject;
+        Type = type;
+    }
+}
+    
+public enum TileType
+{
+    Regular,
+    Hole,
+    Border,
+    Obstacle
+}
