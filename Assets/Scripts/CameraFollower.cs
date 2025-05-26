a50e295d0ce29b3e3aa9ba6071d292f76fdd28d2
Oleksandr Novik <alexandr.novik2@gmail.com>
2025-05-26 18:52:28 +0300

    Camera effects

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -1,3 +1,4 @@
+using Cube;
 using DG.Tweening;
 using Tiles;
 using UnityEngine;
@@ -5,19 +6,27 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     [SerializeField] private Transform Target;
-
-    private const float FollowDuration = 0.5f;
+    [SerializeField] private CubeManager CubeManager;
     
+    private const float AnimationDuration = 0.5f;
+
+    private Camera _camera;
+    
+    private Vector3 _initPosition;
     private Vector3 _positionDelta;
 
     private bool _isIdle = true;
 
-    private Vector3 _initPosition;
-    
+    private void Awake()
+    {
+        CubeManager.OnCubeDestroyed += ZoomOut;
+    }
+
     private void Start()
     {
         var tilePaletteManager = TilePaletteManager.GetInstance();
-        GetComponent<Camera>().backgroundColor = tilePaletteManager.GetPalette()[3];
+        _camera = GetComponent<Camera>();
+        _camera.backgroundColor = tilePaletteManager.GetPalette()[3];
         _positionDelta = transform.position - Target.position;
         _initPosition = transform.position;
     }
@@ -25,24 +34,30 @@ public class CameraFollower : MonoBehaviour
     public void EnableCameraIdle()
     {
         _isIdle = true;
-        transform.DOMove(_initPosition, FollowDuration);
+        transform.DOMove(_initPosition, AnimationDuration);
+        ZoomOut();
     }
 
     public void DisableCameraIdle()
     {
         _isIdle = false;
-        transform.DOMove(_initPosition, FollowDuration);
+        transform.DOMove(_initPosition, AnimationDuration * 2f);
+        ZoomIn();
     }
+
+    private void ZoomIn() => _camera.DOOrthoSize(10, AnimationDuration);
+
+    private void ZoomOut() => _camera.DOOrthoSize(12, AnimationDuration);
 
     private void Update()
     {
         if (_isIdle)
         {
-            transform.position += new Vector3(1, 0, 1) * (Mathf.Sqrt(2) * 0.875f * Time.deltaTime);
+            transform.DOMove(transform.position + new Vector3(1f, 0f, 1f) * 0.6f, 1f).SetEase(Ease.Linear);
         }
         else
         {
-            transform.DOMove(Target.position + _positionDelta, FollowDuration);
+            transform.DOMove(Target.position + _positionDelta, AnimationDuration);
         }
     }
 }
